049ebd5b2e3eefa6aeb9124f361efacc0f621118
tinkertinker393-rgb <tinkertinker393@gmail.com>
2025-11-22 10:14:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~UdpMqF", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254739320033", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
